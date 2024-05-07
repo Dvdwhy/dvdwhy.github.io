85384e46fdbf4a8f959405f112d9b1a5404794b9
dvdwhy <davidwahyu1212@gmail.com>
2024-05-07 16:35:55 +0700

    Upload New Files

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* ====================================================Typing Animation =========================================*/
+var typed = new Typed(".typing", {
+    strings: ["", "Front End Development", "Mobile Development", "Web Development"],
+    typeSpeed: 70,
+    BackSpeed: 70,
+    loop: true
+})
+
+/* ====================================================Aside =========================================*/
